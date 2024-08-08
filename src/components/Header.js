75c20edf650db905b35678a5b9116571cdca79bf
Sama Durani <duranisama@gmail.com>
2024-08-08 15:00:12 -0400

    complete lab

---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+
+
+function Header({onDarkModeClick, isDarkMode}) {
+    
+    return (
+        <header>
+            <h2>Shopster</h2>
+            <button onClick={onDarkModeClick}>
+            {isDarkMode ? "Dark" : "Light"} Mode
+            </button>
+        </header>
+    )
+
+}
+
+export default Header;
